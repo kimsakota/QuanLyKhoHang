f7854faeb157abb6cfc9b20eeac3cb45f21bf379
kimsakota <161341093+kimsakota@users.noreply.github.com>
2025-11-05 06:47:43 +0700

    f

--- ViewModels/Pages/LienHe/KhachHangViewModel.cs
+++ ViewModels/Pages/LienHe/KhachHangViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UiDesktopApp1.Models;
 
 namespace UiDesktopApp1.ViewModels.Pages.LienHe
 {
     public partial class KhachHangViewModel : ObservableObject
     {
+
+        public ObservableCollection<CustomerModel> Customers { get; } = new();
 
     }
 }
