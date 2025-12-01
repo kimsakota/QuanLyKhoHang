c1a85872a5568847313e90550c1bdec300ba319f
kimsakota <161341093+kimsakota@users.noreply.github.com>
2025-12-02 02:33:24 +0700

    1

--- QuanLyKho.API/Program.cs
+++ QuanLyKho.API/Program.cs
@@ -1,4 +1,5 @@
-namespace QuanLyKho.API;
+
+namespace QuanLyKho.Api;
 
 public class Program
 {
@@ -7,24 +8,25 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment())
         {
-            app.UseExceptionHandler("/Home/Error");
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
-        app.UseStaticFiles();
-
-        app.UseRouting();
 
         app.UseAuthorization();
 
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.MapControllers();
 
         app.Run();
     }
--- QuanLyKho.Server/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QuanLyKho.Server.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
